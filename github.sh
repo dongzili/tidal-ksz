4bdf3db78812254c633dfcb42607de679062039b
fleaf5 <dongzli@hotmail.com>
2016-03-23 23:01:05 -0400

    3.23.3 with new figure

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "3.23.2 with figure"
+git commit -m "3.23.3 with new figure"
 
-git remote add 3.23.2 https://github.com/fleaf5/tidal-ksz.git
-git push -u 3.23.2 master
+git remote add 3.23.3 https://github.com/fleaf5/tidal-ksz.git
+git push -u 3.23.3 master
 
