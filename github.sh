0af3f305fd0727882f78b25f522af4ace9a27677
fleaf5 <dongzli@hotmail.com>
2016-03-23 20:25:14 -0400

    3.23 without figure

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "3.22 trial reorganize, incomplete"
+git commit -m "3.23 without figure"
 
-git remote add 3.22 https://github.com/fleaf5/tidal-ksz.git
-git push -u 3.22 master
+git remote add 3.23 https://github.com/fleaf5/tidal-ksz.git
+git push -u 3.23 master
 
