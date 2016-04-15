78452e9888ca0fb6d420caccb00bda22db7d5bba
fleaf5 <dongzli@hotmail.com>
2016-04-15 15:57:16 -0400

    4.15 new abstract

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "3.23.3 with new figure"
+git commit -m "4.15 new abstract"
 
-git remote add 3.23.3 https://github.com/fleaf5/tidal-ksz.git
-git push -u 3.23.3 master
+git remote add 4.15 https://github.com/fleaf5/tidal-ksz.git
+git push -u 4.15 master
 
