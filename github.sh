3139ab9efdc81c3cc06ae89e81448e3a242d3476
fleaf5 <dongzli@hotmail.com>
2016-04-15 10:26:54 -0400

    4.15 3d reconstruction

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
 git commit -m "4.15 3d reconstruction"
-#git pull https://github.com/PerimeterInstitute/tidal-ksz
+git pull https://github.com/PerimeterInstitute/tidal-ksz
 git remote add 4.15 https://github.com/PerimeterInstitute/tidal-ksz
 git push -u 4.15 master
 
