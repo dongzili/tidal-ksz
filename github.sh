ad338197d695342cebe19f7fc8b7a5f9c9f65b6b
fleaf5 <dongzli@hotmail.com>
2016-04-15 19:38:52 -0400

    4.15.2 3D tides

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "4.15 new abstract"
+git commit -m "4.15.2 3D tides"
 
-git remote add 4.15 https://github.com/fleaf5/tidal-ksz.git
-git push -u 4.15 master
+git remote add 4.15.2 https://github.com/fleaf5/tidal-ksz.git
+git push -u 4.15.2 master
 
